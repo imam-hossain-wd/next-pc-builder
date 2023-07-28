0fdd101611c437b2b877cdbd9ba48037ce48dd4f
imam-hossain <zunayedemon6t9@gmail.com>
2023-07-28 16:20:44 +0600

    create redux store and wrapper next app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,19 @@
 
 import Footer from '@/components/ui/Footer';
 import Navbar from '@/components/ui/Navbar';
+import { store } from '@/redux/store';
 import '@/styles/globals.css'
 import { SessionProvider } from "next-auth/react"
+import { Provider } from 'react-redux';
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
   return(
     <SessionProvider session={pageProps.session}>
+       <Provider store={store}>
       <Navbar/>
       <Component {...pageProps} />
       <Footer/>
+       </Provider>
     </SessionProvider>
    
   );
