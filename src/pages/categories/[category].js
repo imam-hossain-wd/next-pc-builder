324ea1dbdda2eb1dc56a3c96975acd3a4d723273
imam-hossain <zunayedemon6t9@gmail.com>
2023-08-14 21:21:59 +0600

    category cpage details page implement successfully

--- src/pages/categories/[category].js
+++ src/pages/categories/[category].js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import image from '../../assets/image/desktop-carousel4-280922.webp'
+import Link from 'next/link';
 
 const CategoryPage = ({ products }) => {
   const router = useRouter();
@@ -9,25 +10,28 @@ const CategoryPage = ({ products }) => {
   return (
     <div>
       <h1 className='text-center font-bold text-3xl my-5 text-black capitalize'>{category} Category</h1>
-
       <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-5 gap-5 ml-20 w-[90%] mx-auto'>
-      {
+        {
           products.map((product) => (
-          <div key={product._id}>
-            <div className="card w-80 h-[400px] shadow-xl border-gray-600 border-2 text-black  ">
-              <figure className="px-10 pt-10">
-                <Image src={image} width={300} height={300}alt="Shoes" className="rounded-lg w-60 h-48" />
-              </figure>
-              <div className="card-body  ">
-                <h2 className="text-lg font-bold">{product.productName}</h2>
-                <h2 className="text-sm">{product.category}</h2>
-                <h2 className="text-sm">{product.price}</h2>
+
+            <div key={product._id}>
+              <Link href={`/components/${product._id}`}>
+                <div className="card w-80 h-[400px] shadow-xl border-gray-600 border-2 text-black  ">
+                  <figure className="px-10 pt-10">
+                    <Image src={image} width={300} height={300} alt="Shoes" className="rounded-lg w-60 h-48" />
+                  </figure>
+                  <div className="card-body  ">
+                    <h2 className="text-lg font-bold">{product.productName}</h2>
+                    <h2 className="text-sm">{product.category}</h2>
+                    <h2 className="text-sm">{product.price}</h2>
+                  </div>
                 </div>
+              </Link>
             </div>
-          </div>
-        ))
-       
-      } </div>
+
+          ))
+
+        } </div>
 
     </div>
   );
